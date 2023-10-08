f6616d5217430ef52d4f149c284026e284ff3b34
2Sravs <sravani.chekuri1243#gmail.com>
2023-10-08 22:27:18 +0530

    firstcommit

--- obj/Debug/net6.0/PracticeApp1.AssemblyInfo.cs
+++ obj/Debug/net6.0/PracticeApp1.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("bca1823d-3fd7-4782-a0f3-6251c3ced187")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("PracticeApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
